870a59a1f8e19f5e008ba88005594dbd6a735b03
djzwns <hjjshg@game.hoseo.edu>
2017-04-03 16:38:19 +0900

    간단한 씬 이동 스크립트 추가.

---
+++ Assets/Scripts/OpenScene.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class OpenScene : MonoBehaviour {
+
+    public void ButtonOpenMain()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void ButtonOpenLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+    public void ButtonOpenGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ButtonGameExit()
+    {
+        Application.Quit();
+    }
+}
